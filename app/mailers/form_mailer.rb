e1195fe6b8f6ee05d3f6e069348e13d49adec035
Guymon Hall <guymon.hall@guymonhall.CS.unlv.edu>
2016-09-21 15:43:30 -0700

    increased asterisk font size; fixed Chrome date-field issue; added brief instruction note; cc'd requester when form is emailed

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -4,6 +4,7 @@ class FormMailer < ApplicationMailer
 
     headers = {
         to: departments[@params[:department].to_i][2],
+        cc: @params[:requester_email],
         from: "#{@params[:requester_first_name]} #{@params[:requester_last_name]} <#{@params[:requester_email]}>",
         subject: "Hourly Contract Request - #{@params[:employee_first_name]} #{@params[:employee_middle_initial]} #{@params[:employee_last_name]}",
         reply_to: @params[:requester_email]
@@ -17,6 +18,7 @@ class FormMailer < ApplicationMailer
 
     headers = {
         to: departments[@params[:department].to_i][2],
+        cc: @params[:requester_email],
         from: "#{@params[:requester_first_name]} #{@params[:requester_last_name]} <#{@params[:requester_email]}>",
         subject: "LOA/LOB Contract Request - #{@params[:employee_first_name]} #{@params[:employee_middle_initial]} #{@params[:employee_last_name]}",
         reply_to: @params[:requester_email]
