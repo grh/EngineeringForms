faa64de37dea6cfe3717ad0d9f2a85bf796399e6
Guymon Hall <guymon.hall@guymonhall.CS.unlv.edu>
2016-09-21 16:16:15 -0700

    disabled auto starttls for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
