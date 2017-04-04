bb945ba51bedc3ee217e86a61abb4c282b68bda1
Guymon Hall <guymon.hall@guymonhall.CS.UNLV.EDU>
2017-04-04 14:04:55 -0700

    Update emails to org accts

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,15 +3,15 @@ class ApplicationMailer < ActionMailer::Base
 
   def departments
     return [
-        [ 'COE Dean\'s Office', 0, ['tara.mullin@unlv.edu', 'marjorie.aylor@unlv.edu'] ],
-        [ 'COE Advising Center', 1, ['tara.mullin@unlv.edu', 'maura.erickson@unlv.edu'] ],
-        [ 'Transportation Research Center (TRC)', 2, ['tara.mullin@unlv.edu'] ],
-        [ 'Civil & Env. Engr. & Const. (CEEC)', 3, ['larese.patillo@unlv.edu', 'tara.mullin@unlv.edu'] ],
-        [ 'Computer Science (CS)', 4, ['tara.mullin@unlv.edu', 'leslie.crysler@unlv.edu'] ],
-        [ 'Electrical & Computer Engr. (ECE)', 5, ['tara.mullin@unlv.edu', 'cameron.williams@unlv.edu'] ],
-        [ 'Entertainment Engr. & Design (EED)', 6, ['tara.mullin@unlv.edu', 'marjorie.aylor@unlv.edu'] ],
-        [ 'Mechanical Engineering (ME)', 7, ['tara.mullin@unlv.edu', 'terrel.parker@unlv.edu'] ],
-        [ 'Center for Energy Research (CER)', 8, ['tara.mullin@unlv.edu', 'joan.conway@unlv.edu'] ],
+        [ 'COE Dean\'s Office', 0, ['EGR-Dean-Staff@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'COE Advising Center', 1, [ 'kimberlee.ornbaum@unlv.edu'] ],
+        [ 'Transportation Research Center (TRC)', 2, ['larese.patillo@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'Civil & Env. Engr. & Const. (CEEC)', 3, ['CEEC-Staff@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'Computer Science (CS)', 4, ['CS-Staff@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'Electrical & Computer Engr. (ECE)', 5, ['ECE-Staff@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'Entertainment Engr. & Design (EED)', 6, ['EGR-Dean-Staff@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'Mechanical Engineering (ME)', 7, ['ME-Staff@unlv.edu', 'tara.mullin@unlv.edu'] ],
+        [ 'Center for Energy Research (CER)', 8, ['tasha.ramos@unlv.edu', 'tara.mullin@unlv.edu', 'Rosangela.Wacaser@unlv.edu'] ],
         [ 'Aerospace Studies (AFROTC)', 9, ['jessica.sawicki@unlv.edu'] ],
         [ 'Military Science (ARMY)', 10, ['jessica.sawicki@unlv.edu'] ]
         #[ 'Guymon\'s Office', 0, ['guymon.hall@unlv.edu'] ]
